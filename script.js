e9856d3675e466785854490c4a2aeb0c71eb0ed8
debbyne <kinyadebbyne@gmail.com>
2021-12-26 16:30:12 +0300

    add jquery effects

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 $(document).ready(function (){
-    $("img1").show();
+   $(".img1-img").click(function(){
+       $(".img1-paragragh").show();
+      $(".this").hide();
+   })
+$(".img1-paragragh").click(function () {
+    $(".img1-img").show();
+    $(this).hide();
+}) 
+   $(".img2-img").click(function(){
+       $(".img2-paragragh").show();
+       $("this").hide();
+   })
     
-}
+   $(".img2-paragragh").click(function () {
+       $(".img2-img").sjow();
+       $(this).hide();
+   })
+
+   $(".img3-img").click(function(){
+       $(".img3-paragragh").show();
+       $(this).hide();
+   })
+   $(".img3-paragragh").click(function() {
+    $(".img2-img").sjow();
+    $(this).hide();
+   })
+
+})
