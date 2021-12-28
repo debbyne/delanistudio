18d5fdc04d5a608a6a7a6c65507d6292b509ff7b
debbyne <kinyadebbyne@gmail.com>
2021-12-28 12:31:39 +0300

    add social media icons

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-$(document).ready(function (){
-   $(".img1-img").click(function(){
-       $(".img1-paragragh").show();
-      $(".this").hide();
-   })
-$(".img1-paragragh").click(function () {
+$(document).ready(function () {
+  $(".img1-img").click(function () {
+    $(".img1-paragragh").show();
+    $(".this").hide();
+  });
+  $(".img1-paragragh").click(function () {
     $(".img1-img").show();
     $(this).hide();
-}) 
-   $(".img2-img").click(function(){
-       $(".img2-paragragh").show();
-       $("this").hide();
-   })
-    
-   $(".img2-paragragh").click(function () {
-       $(".img2-img").sjow();
-       $(this).hide();
-   })
+  });
+  $(".img2-img").click(function () {
+    $(".img2-paragragh").show();
+    $("this").hide();
+  });
 
-   $(".img3-img").click(function(){
-       $(".img3-paragragh").show();
-       $(this).hide();
-   })
-   $(".img3-paragragh").click(function() {
+  $(".img2-paragragh").click(function () {
     $(".img2-img").sjow();
     $(this).hide();
-   })
+  });
 
-})
+  $(".img3-img").click(function () {
+    $(".img3-paragragh").show();
+    $(this).hide();
+  });
+  $(".img3-paragragh").click(function () {
+    $(".img2-img").sjow();
+    $(this).hide();
+  });
+});
