976b7b3ab9fb5c9487274f7f368996718f3d8f17
debbyne <kinyadebbyne@gmail.com>
2022-01-04 08:52:34 +0300

    add changes in layout

--- script.js
+++ script.js
@@ -19,3 +19,62 @@ $(document).ready(function(){
     $(".del1").hide()
   })
 })
+$(document).ready(function(){
+  $(".img-port_2").mouseenter(function(){
+    $(".del2").show()
+  })
+  $(".img-port_2").mouseleave(function(){
+    $(".del2").hide()
+  })
+})
+$(document).ready(function(){
+  $(".img-port_3").mouseenter(function(){
+    $(".del3").show()
+  })
+  $(".img-port_3").mouseleave(function(){
+    $(".del3").hide()
+  })
+})
+$(document).ready(function(){
+  $(".img-port_4").mouseenter(function(){
+    $(".del4").show()
+  })
+  $(".img-port_4").mouseleave(function(){
+    $(".del4").hide()
+  })
+})
+$(document).ready(function(){
+  $(".img-port_5").mouseenter(function(){
+    $(".del5").show()
+  })
+  $(".img-port_5").mouseleave(function(){
+    $(".del5").hide()
+  })
+})
+$(document).ready(function(){
+  $(".img-port_6").mouseenter(function(){
+    $(".del6").show()
+  })
+  $(".img-port_6").mouseleave(function(){
+    $(".del6").hide()
+  })
+})
+$(document).ready(function(){
+  $(".img-port_7").mouseenter(function(){
+    $(".del7").show()
+  })
+  $(".img-port_7").mouseleave(function(){
+    $(".del7").hide()
+  })
+})
+$(document).ready(function(){
+  $(".img-port_8").mouseenter(function(){
+    $(".del8").show()
+  })
+  $(".img-port_8").mouseleave(function(){
+    $(".del8").hide()
+  })
+})
+
+
+
